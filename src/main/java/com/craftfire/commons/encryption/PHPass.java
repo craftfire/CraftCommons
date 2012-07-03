2f252d75902c09ef073a5d4c70b2de4094d49455
Wolf480pl <wolf480@interia.pl>
2012-07-03 08:42:12 +0200

    Removed prank header from PHPass.java; Converted PHPass.java and BCrypt.java to Oracle coding standards; Added CraftFire license notice at the top of BCrypt.java

--- src/main/java/com/craftfire/commons/encryption/PHPass.java
+++ src/main/java/com/craftfire/commons/encryption/PHPass.java
@@ -1,11 +1,4 @@
 /*
- * This file was developed by Wolf480pl before joining CraftFire.
- * Wolf480pl allows CraftFire to do with this file anything they want and redistribute it on any license, with following limitations:
- * - ONLY Wolf480pl may remove this note
- * - NOONE may convert this file to Oracle formatting standards without Wolf480pl's permission.
- * CraftFire's license should be just below.                                                                                                                                                      THIS IS A PRANK :D You may remove this note anytime you want.
- */
-/*
  * This file is part of CraftCommons.
  *
  * Copyright (c) 2011-2012, CraftFire <http://www.craftfire.com/>
@@ -32,90 +25,93 @@ import java.security.SecureRandom;
 import java.util.Random;
 
 public class PHPass {
-	static String itoa64 = "./0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	int iteration_count_log2;
-	SecureRandom random_gen;
-	public PHPass(int iteration_count_log2){
-		if(iteration_count_log2 < 4 || iteration_count_log2 > 31)
-			iteration_count_log2 = 8;
-		this.iteration_count_log2 = iteration_count_log2;
-		this.random_gen = new SecureRandom(); 
-	}
-	private String encode64(byte[] src, int count){
-		int i, value;
-		String output = "";
-		i = 0;
-		do{
-			value = src[i]  + (src[i] < 0 ? 256 : 0);
-			++i;
-			output += itoa64.charAt(value & 63);
-			if(i < count){
-				value |= (src[i] + (src[i] < 0 ? 256 : 0)) << 8;
-			}
-			output += itoa64.charAt((value >> 6) & 63);
-			if(i++ >= count) break;
-			if(i < count){
-				value |= (src[i] + (src[i] < 0 ? 256 : 0)) << 16;
-			}
-			output += itoa64.charAt((value >> 12) & 63);
-			if(i++ >= count) break;
-			output += itoa64.charAt((value >> 18) & 63);
-		}while(i < count);
-		return output;
-	}
-	private String crypt_private(String password, String setting){
-		String output = "*0";
-		if(((setting.length() < 2 )? setting : setting.substring(0, 2)).equalsIgnoreCase(output))
-			output = "*1";
-		String id = (setting.length() < 3) ? setting : setting.substring(0, 3);
-		if(!(id.equals("$P$") || id.equals("$H$")))
-			return output;
-		int count_log2 = itoa64.indexOf(setting.charAt(3));
-		if (count_log2 < 7 || count_log2 > 30)
-			return output;
-		int count = 1 << count_log2;
-		String salt = setting.substring(4, 4+8);
-		if(salt.length() != 8)
-			return output;
-		MessageDigest md;
-		try {
-			md = MessageDigest.getInstance("MD5");
-		} catch (NoSuchAlgorithmException e) {
-			e.printStackTrace();
-			return output;
-		}
-		byte[] hash = md.digest((salt + password).getBytes());
-		do{
-			byte[] t = new byte[hash.length + password.length()];
-			System.arraycopy(hash, 0, t, 0, hash.length);
-			System.arraycopy(password.getBytes(), 0, t, hash.length, password.length());
-			hash = md.digest(t);
-		} while(--count > 0);
-		output = setting.substring(0, 12);
-		output += this.encode64(hash, 16);
-		return output;
-	}
-	private String gensalt_private(String input){
-		String output = "$P$";
-		output += itoa64.charAt(Math.min(iteration_count_log2 + 5, 30));
-		output += encode64(input.getBytes(), 6);
-		return output;
-	}
-	public String crypt(String password, String setting) {
-		return crypt_private(password, setting);
-	}
-	public String gensalt(String input) {
-		return gensalt_private(input);
-	}
-	public String gensalt(byte[] input) {
-		return gensalt_private(new String(input));
-	}
-	public String gensalt(){
-		byte random[] = new byte[6];
-		random_gen.nextBytes(random);
-		return gensalt(random);
-	}
-	public Random getRandomGen(){
-		return random_gen;
-	}
+    static String itoa64 =
+    		"./0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    int iteration_count_log2;
+    SecureRandom random_gen;
+    public PHPass(int iteration_count_log2){
+        if (iteration_count_log2 < 4 || iteration_count_log2 > 31)
+            iteration_count_log2 = 8;
+        this.iteration_count_log2 = iteration_count_log2;
+        this.random_gen = new SecureRandom(); 
+    }
+    private String encode64(byte[] src, int count) {
+        int i, value;
+        String output = "";
+        i = 0;
+        do {
+            value = src[i]  + (src[i] < 0 ? 256 : 0);
+            ++i;
+            output += itoa64.charAt(value & 63);
+            if (i < count) {
+                value |= (src[i] + (src[i] < 0 ? 256 : 0)) << 8;
+            }
+            output += itoa64.charAt((value >> 6) & 63);
+            if (i++ >= count) break;
+            if (i < count) {
+                value |= (src[i] + (src[i] < 0 ? 256 : 0)) << 16;
+            }
+            output += itoa64.charAt((value >> 12) & 63);
+            if (i++ >= count) break;
+            output += itoa64.charAt((value >> 18) & 63);
+        } while (i < count);
+        return output;
+    }
+    private String crypt_private(String password, String setting) {
+        String output = "*0";
+        if (((setting.length() < 2 )? setting : setting.substring(0, 2))
+        		.equalsIgnoreCase(output))
+            output = "*1";
+        String id = (setting.length() < 3) ? setting : setting.substring(0, 3);
+        if (!(id.equals("$P$") || id.equals("$H$")))
+            return output;
+        int count_log2 = itoa64.indexOf(setting.charAt(3));
+        if (count_log2 < 7 || count_log2 > 30)
+            return output;
+        int count = 1 << count_log2;
+        String salt = setting.substring(4, 4+8);
+        if (salt.length() != 8)
+            return output;
+        MessageDigest md;
+        try {
+            md = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return output;
+        }
+        byte[] hash = md.digest((salt + password).getBytes());
+        do {
+            byte[] t = new byte[hash.length + password.length()];
+            System.arraycopy(hash, 0, t, 0, hash.length);
+            System.arraycopy(password.getBytes(), 0, t, hash.length,
+            		password.length());
+            hash = md.digest(t);
+        } while (--count > 0);
+        output = setting.substring(0, 12);
+        output += this.encode64(hash, 16);
+        return output;
+    }
+    private String gensalt_private(String input) {
+        String output = "$P$";
+        output += itoa64.charAt(Math.min(iteration_count_log2 + 5, 30));
+        output += encode64(input.getBytes(), 6);
+        return output;
+    }
+    public String crypt(String password, String setting) {
+        return crypt_private(password, setting);
+    }
+    public String gensalt(String input) {
+        return gensalt_private(input);
+    }
+    public String gensalt(byte[] input) {
+        return gensalt_private(new String(input));
+    }
+    public String gensalt() {
+        byte random[] = new byte[6];
+        random_gen.nextBytes(random);
+        return gensalt(random);
+    }
+    public Random getRandomGen() {
+        return random_gen;
+    }
 }
