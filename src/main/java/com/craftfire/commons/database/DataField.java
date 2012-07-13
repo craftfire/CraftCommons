718421c3c465adbee7662673f567174d04e0e95d
Contex <contexmoh@gmail.com>
2012-07-13 11:08:59 +0200

    Forgot to import the DataManager class.

--- src/main/java/com/craftfire/commons/database/DataField.java
+++ src/main/java/com/craftfire/commons/database/DataField.java
@@ -19,6 +19,9 @@
  */
 package com.craftfire.commons.database;
 
+import com.craftfire.commons.enums.FieldType;
+import com.craftfire.commons.managers.DataManager;
+
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.nio.ByteBuffer;
@@ -30,9 +33,6 @@ import java.sql.Types;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.util.Date;
-
-import com.craftfire.commons.DataManager;
-import com.craftfire.commons.enums.FieldType;
 
 public class DataField {
     private final String name, table /* , type */;
