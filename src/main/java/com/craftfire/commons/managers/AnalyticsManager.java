f4a1caaec4669b2ddfc861cedd2a1e92d71c2239
Contex <contexmoh@gmail.com>
2012-10-18 11:39:21 +0200

    submit() now throws AnalyticsException instead of returning false.

--- src/main/java/com/craftfire/commons/managers/AnalyticsManager.java
+++ src/main/java/com/craftfire/commons/managers/AnalyticsManager.java
@@ -21,30 +21,28 @@ package com.craftfire.commons.managers;
 
 import com.craftfire.commons.Util;
 import com.craftfire.commons.classes.AnalyticsData;
+import com.craftfire.commons.exceptions.AnalyticsException;
 
 import java.net.URL;
 
 public class AnalyticsManager {
     private URL url;
-    private String name, version, error = "No errors found.";
-    private AnalyticsData data = new AnalyticsData();
+    private AnalyticsData data;
     private LoggingManager loggingManager = new LoggingManager("CraftFire.AnalyticsManager", "[AnalyticsManager]");
 
     public AnalyticsManager(URL url, String name, String version) {
         this.url = url;
-        this.name = name;
-        this.version = version;
+        this.data = new AnalyticsData(name, version);
     }
 
-    public boolean submit() {
+    public void submit() throws AnalyticsException {
         if (Util.isURLOnline(getURL())) {
             //TODO: Make it submit.
-            return true;
         }
-        setError(getURL().toString() + " did not return HTTP Status 200, status returned was: " +
-                 Util.getResponseCode(getURL()) + ".");
-        getLogging().error(getError());
-        return false;
+        String error = getURL().toString() + " did not return HTTP Status 200, status returned was: " +
+                       Util.getResponseCode(getURL()) + ".";
+        getLogging().error(error);
+        throw new AnalyticsException(this, error);
     }
 
     public LoggingManager getLogging() {
@@ -69,29 +67,5 @@ public class AnalyticsManager {
 
     public void setURL(URL url) {
         this.url = url;
-    }
-
-    public String getName() {
-       return this.name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getVersion() {
-        return this.version;
-    }
-
-    public void setVersion(String version) {
-        this.version = version;
-    }
-
-    public String getError() {
-        return this.error;
-    }
-
-    protected void setError(String error) {
-        this.error = error;
     }
 }
