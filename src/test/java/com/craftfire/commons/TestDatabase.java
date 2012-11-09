15ea004f6c86eb4e048370328c7661d5efa0abe6
Wolf480pl <wolf480@interia.pl>
2012-11-09 15:01:26 +0100

    Finished TestDatabase - it tests all the different field types in H2.

--- src/test/java/com/craftfire/commons/TestDatabase.java
+++ src/test/java/com/craftfire/commons/TestDatabase.java
@@ -579,6 +579,440 @@ public class TestDatabase {
         assertNotNull(row.getStringField(name));
     }
 
+    @Test
+    public void testLVBin() throws SQLException {
+        final String name = "lvbin";
+        DataRow row = datamanager.getResults("SELECT `" + name + "` FROM `" + table + "` LIMIT 1").getFirstResult();
+
+        // DataRow.getField()
+        DataField field = row.get(name);
+        assertNotNull(field.getBigInt());
+        assertTrue(field.getBool());
+        assertNotNull(field.getBytes());
+        assertThat(field.getDouble(), not(equalTo(0d)));
+        assertThat(field.getFloat(), not(equalTo(0f)));
+        assertThat(field.getInt(), not(equalTo(0)));
+        assertThat(field.getLong(), not(equalTo(0L)));
+        assertNotNull(field.getString());
+
+        // DataManager.getField()
+        field = datamanager.getField(FieldType.UNKNOWN, table, name, "1");
+        assertNotNull(field.getBigInt());
+        assertTrue(field.getBool());
+        assertNotNull(field.getBytes());
+        assertThat(field.getDouble(), not(equalTo(0d)));
+        assertThat(field.getFloat(), not(equalTo(0f)));
+        assertThat(field.getInt(), not(equalTo(0)));
+        assertThat(field.getLong(), not(equalTo(0L)));
+        assertNotNull(field.getString());
+
+        // DataManager.get<Kinda>Field()
+        assertNotNull(datamanager.getBinaryField(table, name, "1"));
+        assertNotNull(datamanager.getBlobField(table, name, "1"));
+        assertNotNull(datamanager.getStringField(table, name, "1"));
+
+        // DataRow.get<Kinda>Field()
+        assertNotNull(row.getBigIntField(name));
+        assertTrue(row.getBoolField(name));
+        assertNotNull(row.getBinaryField(name));
+        assertThat(row.getDoubleField(name), not(equalTo(0d)));
+        assertThat(row.getFloatField(name), not(equalTo(0f)));
+        assertThat(row.getIntField(name), not(equalTo(0)));
+        assertThat(row.getLongField(name), not(equalTo(0L)));
+        assertNotNull(row.getStringField(name));
+    }
+
+    @Test
+    public void testLVChar() throws SQLException {
+        final String name = "lvchar";
+        DataRow row = datamanager.getResults("SELECT `" + name + "` FROM `" + table + "` LIMIT 1").getFirstResult();
+
+        // DataRow.getField()
+        DataField field = row.get(name);
+        assertTrue(field.getBool());
+        assertNotNull(field.getBytes());
+        assertNotNull(field.getString());
+
+        // DataManager.getField()
+        field = datamanager.getField(FieldType.UNKNOWN, table, name, "1");
+        assertTrue(field.getBool());
+        assertNotNull(field.getBytes());
+        assertNotNull(field.getString());
+
+        // DataManager.get<Kinda>Field()
+        assertNotNull(datamanager.getBlobField(table, name, "1"));
+        assertNotNull(datamanager.getStringField(table, name, "1"));
+
+        // DataRow.get<Kinda>Field()
+        assertTrue(row.getBoolField(name));
+        assertNotNull(row.getBinaryField(name));
+        assertNotNull(row.getStringField(name));
+    }
+
+    @Test
+    public void testNumeric() throws SQLException {
+        final String name = "numeric";
+        DataRow row = datamanager.getResults("SELECT `" + name + "` FROM `" + table + "` LIMIT 1").getFirstResult();
+
+        // DataRow.getField()
+        DataField field = row.get(name);
+        assertNotNull(field.getBigInt());
+        assertTrue(field.getBool());
+        assertNotNull(field.getBytes());
+        assertNotNull(field.getDecimal());
+        assertThat(field.getDouble(), not(equalTo(0d)));
+        assertThat(field.getFloat(), not(equalTo(0f)));
+        assertThat(field.getInt(), not(equalTo(0)));
+        assertThat(field.getLong(), not(equalTo(0L)));
+        assertNotNull(field.getString());
+
+        // DataManager.getField()
+        field = datamanager.getField(FieldType.UNKNOWN, table, name, "1");
+        assertNotNull(field.getBigInt());
+        assertTrue(field.getBool());
+        assertNotNull(field.getBytes());
+        assertNotNull(field.getDecimal());
+        assertThat(field.getDouble(), not(equalTo(0d)));
+        assertThat(field.getFloat(), not(equalTo(0f)));
+        assertThat(field.getInt(), not(equalTo(0)));
+        assertThat(field.getLong(), not(equalTo(0L)));
+        assertNotNull(field.getString());
+
+        // DataManager.get<Kinda>Field()
+        assertTrue(datamanager.getBooleanField(table, name, "1"));
+        assertNotNull(datamanager.getBinaryField(table, name, "1"));
+        assertNotNull(datamanager.getBlobField(table, name, "1"));
+        assertThat(datamanager.getDoubleField(table, name, "1"), not(equalTo(0d)));
+        assertThat(datamanager.getIntegerField(table, name, "1"), not(equalTo(0)));
+        assertNotNull(datamanager.getStringField(table, name, "1"));
+
+        // DataRow.get<Kinda>Field()
+        assertNotNull(row.getBigIntField(name));
+        assertTrue(row.getBoolField(name));
+        assertNotNull(row.getBinaryField(name));
+        assertNotNull(row.getDecimalField(name));
+        assertThat(row.getDoubleField(name), not(equalTo(0d)));
+        assertThat(row.getFloatField(name), not(equalTo(0f)));
+        assertThat(row.getIntField(name), not(equalTo(0)));
+        assertThat(row.getLongField(name), not(equalTo(0L)));
+        assertNotNull(row.getStringField(name));
+    }
+
+    @Test
+    public void testReal() throws SQLException {
+        final String name = "real";
+        DataRow row = datamanager.getResults("SELECT `" + name + "` FROM `" + table + "` LIMIT 1").getFirstResult();
+
+        // DataRow.getField()
+        DataField field = row.get(name);
+        assertNotNull(field.getBigInt());
+        assertTrue(field.getBool());
+        assertNotNull(field.getBytes());
+        assertNotNull(field.getDecimal());
+        assertThat(field.getDouble(), not(equalTo(0d)));
+        assertThat(field.getFloat(), not(equalTo(0f)));
+        assertThat(field.getInt(), not(equalTo(0)));
+        assertThat(field.getLong(), not(equalTo(0L)));
+        assertNotNull(field.getString());
+
+        // DataManager.getField()
+        field = datamanager.getField(FieldType.UNKNOWN, table, name, "1");
+        assertNotNull(field.getBigInt());
+        assertTrue(field.getBool());
+        assertNotNull(field.getBytes());
+        assertNotNull(field.getDecimal());
+        assertThat(field.getDouble(), not(equalTo(0d)));
+        assertThat(field.getFloat(), not(equalTo(0f)));
+        assertThat(field.getInt(), not(equalTo(0)));
+        assertThat(field.getLong(), not(equalTo(0L)));
+        assertNotNull(field.getString());
+
+        // DataManager.get<Kinda>Field()
+        assertTrue(datamanager.getBooleanField(table, name, "1"));
+        assertNotNull(datamanager.getBlobField(table, name, "1"));
+        assertThat(datamanager.getDoubleField(table, name, "1"), not(equalTo(0d)));
+        assertThat(datamanager.getIntegerField(table, name, "1"), not(equalTo(0)));
+        assertNotNull(datamanager.getStringField(table, name, "1"));
+
+        // DataRow.get<Kinda>Field()
+        assertNotNull(row.getBigIntField(name));
+        assertTrue(row.getBoolField(name));
+        assertNotNull(row.getBinaryField(name));
+        assertNotNull(row.getDecimalField(name));
+        assertThat(row.getDoubleField(name), not(equalTo(0d)));
+        assertThat(row.getFloatField(name), not(equalTo(0f)));
+        assertThat(row.getIntField(name), not(equalTo(0)));
+        assertThat(row.getLongField(name), not(equalTo(0L)));
+        assertNotNull(row.getStringField(name));
+    }
+
+    @Test
+    public void testSInt() throws SQLException {
+        final String name = "sint";
+        DataRow row = datamanager.getResults("SELECT `" + name + "` FROM `" + table + "` LIMIT 1").getFirstResult();
+
+        // DataRow.getField()
+        DataField field = row.get(name);
+        assertNotNull(field.getBigInt());
+        assertTrue(field.getBool());
+        assertNotNull(field.getBytes());
+        assertNotNull(field.getDate());
+        assertNotNull(field.getDecimal());
+        assertThat(field.getDouble(), not(equalTo(0d)));
+        assertThat(field.getFloat(), not(equalTo(0f)));
+        assertThat(field.getInt(), not(equalTo(0)));
+        assertThat(field.getLong(), not(equalTo(0L)));
+        assertNotNull(field.getString());
+
+        // DataManager.getField()
+        field = datamanager.getField(FieldType.UNKNOWN, table, name, "1");
+        assertNotNull(field.getBigInt());
+        assertTrue(field.getBool());
+        assertNotNull(field.getBytes());
+        assertNotNull(field.getDate());
+        assertNotNull(field.getDecimal());
+        assertThat(field.getDouble(), not(equalTo(0d)));
+        assertThat(field.getFloat(), not(equalTo(0f)));
+        assertThat(field.getInt(), not(equalTo(0)));
+        assertThat(field.getLong(), not(equalTo(0L)));
+        assertNotNull(field.getString());
+
+        // DataManager.get<Kinda>Field()
+        assertTrue(datamanager.getBooleanField(table, name, "1"));
+        assertNotNull(datamanager.getBinaryField(table, name, "1"));
+        assertNotNull(datamanager.getBlobField(table, name, "1"));
+        assertThat(datamanager.getDoubleField(table, name, "1"), not(equalTo(0d)));
+        assertThat(datamanager.getIntegerField(table, name, "1"), not(equalTo(0)));
+        assertNotNull(datamanager.getStringField(table, name, "1"));
+
+        // DataRow.get<Kinda>Field()
+        assertNotNull(row.getBigIntField(name));
+        assertTrue(row.getBoolField(name));
+        assertNotNull(row.getBinaryField(name));
+        assertNotNull(row.getDateField(name));
+        assertNotNull(row.getDecimalField(name));
+        assertThat(row.getDoubleField(name), not(equalTo(0d)));
+        assertThat(row.getFloatField(name), not(equalTo(0f)));
+        assertThat(row.getIntField(name), not(equalTo(0)));
+        assertThat(row.getLongField(name), not(equalTo(0L)));
+        assertNotNull(row.getStringField(name));
+    }
+
+    @Test
+    public void testTime() throws SQLException {
+        final String name = "time";
+        DataRow row = datamanager.getResults("SELECT `" + name + "` FROM `" + table + "` LIMIT 1").getFirstResult();
+
+        // DataRow.getField()
+        DataField field = row.get(name);
+        assertNotNull(field.getBigInt());
+        assertTrue(field.getBool());
+        assertNotNull(field.getDate());
+        assertNotNull(field.getDecimal());
+        assertThat(field.getDouble(), not(equalTo(0d)));
+        assertThat(field.getFloat(), not(equalTo(0f)));
+        assertThat(field.getInt(), not(equalTo(0)));
+        assertThat(field.getLong(), not(equalTo(0L)));
+        assertNotNull(field.getString());
+
+        // DataManager.getField()
+        field = datamanager.getField(FieldType.UNKNOWN, table, name, "1");
+        assertNotNull(field.getBigInt());
+        assertTrue(field.getBool());
+        assertNotNull(field.getDate());
+        assertNotNull(field.getDecimal());
+        assertThat(field.getDouble(), not(equalTo(0d)));
+        assertThat(field.getFloat(), not(equalTo(0f)));
+        assertThat(field.getInt(), not(equalTo(0)));
+        assertThat(field.getLong(), not(equalTo(0L)));
+        assertNotNull(field.getString());
+
+        // DataManager.get<Kinda>Field()
+        assertNotNull(datamanager.getBlobField(table, name, "1"));
+        assertNotNull(datamanager.getDateField(table, name, "1"));
+        assertNotNull(datamanager.getStringField(table, name, "1"));
+
+        // DataRow.get<Kinda>Field()
+        assertNotNull(row.getBigIntField(name));
+        assertTrue(row.getBoolField(name));
+        assertNotNull(row.getDateField(name));
+        assertNotNull(row.getDecimalField(name));
+        assertThat(row.getDoubleField(name), not(equalTo(0d)));
+        assertThat(row.getFloatField(name), not(equalTo(0f)));
+        assertThat(row.getIntField(name), not(equalTo(0)));
+        assertThat(row.getLongField(name), not(equalTo(0L)));
+        assertNotNull(row.getStringField(name));
+    }
+
+    @Test
+    public void testTimeStamp() throws SQLException {
+        final String name = "timestamp";
+        DataRow row = datamanager.getResults("SELECT `" + name + "` FROM `" + table + "` LIMIT 1").getFirstResult();
+
+        // DataRow.getField()
+        DataField field = row.get(name);
+        assertNotNull(field.getBigInt());
+        assertTrue(field.getBool());
+        assertNotNull(field.getDate());
+        assertNotNull(field.getDecimal());
+        assertThat(field.getDouble(), not(equalTo(0d)));
+        assertThat(field.getFloat(), not(equalTo(0f)));
+        assertThat(field.getInt(), not(equalTo(0)));
+        assertThat(field.getLong(), not(equalTo(0L)));
+        assertNotNull(field.getString());
+
+        // DataManager.getField()
+        field = datamanager.getField(FieldType.UNKNOWN, table, name, "1");
+        assertNotNull(field.getBigInt());
+        assertTrue(field.getBool());
+        assertNotNull(field.getDate());
+        assertNotNull(field.getDecimal());
+        assertThat(field.getDouble(), not(equalTo(0d)));
+        assertThat(field.getFloat(), not(equalTo(0f)));
+        assertThat(field.getInt(), not(equalTo(0)));
+        assertThat(field.getLong(), not(equalTo(0L)));
+        assertNotNull(field.getString());
+
+        // DataManager.get<Kinda>Field()
+        assertNotNull(datamanager.getBlobField(table, name, "1"));
+        assertNotNull(datamanager.getDateField(table, name, "1"));
+        assertNotNull(datamanager.getStringField(table, name, "1"));
+
+        // DataRow.get<Kinda>Field()
+        assertNotNull(row.getBigIntField(name));
+        assertTrue(row.getBoolField(name));
+        assertNotNull(row.getDateField(name));
+        assertNotNull(row.getDecimalField(name));
+        assertThat(row.getDoubleField(name), not(equalTo(0d)));
+        assertThat(row.getFloatField(name), not(equalTo(0f)));
+        assertThat(row.getIntField(name), not(equalTo(0)));
+        assertThat(row.getLongField(name), not(equalTo(0L)));
+        assertNotNull(row.getStringField(name));
+    }
+
+    @Test
+    public void testTInt() throws SQLException {
+        final String name = "tint";
+        DataRow row = datamanager.getResults("SELECT `" + name + "` FROM `" + table + "` LIMIT 1").getFirstResult();
+
+        // DataRow.getField()
+        DataField field = row.get(name);
+        assertNotNull(field.getBigInt());
+        assertTrue(field.getBool());
+        assertNotNull(field.getBytes());
+        assertNotNull(field.getDate());
+        assertNotNull(field.getDecimal());
+        assertThat(field.getDouble(), not(equalTo(0d)));
+        assertThat(field.getFloat(), not(equalTo(0f)));
+        assertThat(field.getInt(), not(equalTo(0)));
+        assertThat(field.getLong(), not(equalTo(0L)));
+        assertNotNull(field.getString());
+
+        // DataManager.getField()
+        field = datamanager.getField(FieldType.UNKNOWN, table, name, "1");
+        assertNotNull(field.getBigInt());
+        assertTrue(field.getBool());
+        assertNotNull(field.getBytes());
+        assertNotNull(field.getDate());
+        assertNotNull(field.getDecimal());
+        assertThat(field.getDouble(), not(equalTo(0d)));
+        assertThat(field.getFloat(), not(equalTo(0f)));
+        assertThat(field.getInt(), not(equalTo(0)));
+        assertThat(field.getLong(), not(equalTo(0L)));
+        assertNotNull(field.getString());
+
+        // DataManager.get<Kinda>Field()
+        assertTrue(datamanager.getBooleanField(table, name, "1"));
+        assertNotNull(datamanager.getBinaryField(table, name, "1"));
+        assertNotNull(datamanager.getBlobField(table, name, "1"));
+        assertThat(datamanager.getDoubleField(table, name, "1"), not(equalTo(0d)));
+        assertThat(datamanager.getIntegerField(table, name, "1"), not(equalTo(0)));
+        assertNotNull(datamanager.getStringField(table, name, "1"));
+
+        // DataRow.get<Kinda>Field()
+        assertNotNull(row.getBigIntField(name));
+        assertTrue(row.getBoolField(name));
+        assertNotNull(row.getBinaryField(name));
+        assertNotNull(row.getDateField(name));
+        assertNotNull(row.getDecimalField(name));
+        assertThat(row.getDoubleField(name), not(equalTo(0d)));
+        assertThat(row.getFloatField(name), not(equalTo(0f)));
+        assertThat(row.getIntField(name), not(equalTo(0)));
+        assertThat(row.getLongField(name), not(equalTo(0L)));
+        assertNotNull(row.getStringField(name));
+    }
+
+    @Test
+    public void testVBin() throws SQLException {
+        final String name = "vbin";
+        DataRow row = datamanager.getResults("SELECT `" + name + "` FROM `" + table + "` LIMIT 1").getFirstResult();
+
+        // DataRow.getField()
+        DataField field = row.get(name);
+        assertNotNull(field.getBigInt());
+        assertTrue(field.getBool());
+        assertNotNull(field.getBytes());
+        assertThat(field.getDouble(), not(equalTo(0d)));
+        assertThat(field.getFloat(), not(equalTo(0f)));
+        assertThat(field.getInt(), not(equalTo(0)));
+        assertThat(field.getLong(), not(equalTo(0L)));
+        assertNotNull(field.getString());
+
+        // DataManager.getField()
+        field = datamanager.getField(FieldType.UNKNOWN, table, name, "1");
+        assertNotNull(field.getBigInt());
+        assertTrue(field.getBool());
+        assertNotNull(field.getBytes());
+        assertThat(field.getDouble(), not(equalTo(0d)));
+        assertThat(field.getFloat(), not(equalTo(0f)));
+        assertThat(field.getInt(), not(equalTo(0)));
+        assertThat(field.getLong(), not(equalTo(0L)));
+        assertNotNull(field.getString());
+
+        // DataManager.get<Kinda>Field()
+        assertNotNull(datamanager.getBinaryField(table, name, "1"));
+        assertNotNull(datamanager.getBlobField(table, name, "1"));
+        assertThat(datamanager.getIntegerField(table, name, "1"), not(equalTo(0)));
+        assertNotNull(datamanager.getStringField(table, name, "1"));
+
+        // DataRow.get<Kinda>Field()
+        assertNotNull(row.getBigIntField(name));
+        assertTrue(row.getBoolField(name));
+        assertNotNull(row.getBinaryField(name));
+        assertThat(row.getDoubleField(name), not(equalTo(0d)));
+        assertThat(row.getFloatField(name), not(equalTo(0f)));
+        assertThat(row.getIntField(name), not(equalTo(0)));
+        assertThat(row.getLongField(name), not(equalTo(0L)));
+        assertNotNull(row.getStringField(name));
+    }
+
+    @Test
+    public void testVChar() throws SQLException {
+        final String name = "vchar";
+        DataRow row = datamanager.getResults("SELECT `" + name + "` FROM `" + table + "` LIMIT 1").getFirstResult();
+
+        // DataRow.getField()
+        DataField field = row.get(name);
+        assertTrue(field.getBool());
+        assertNotNull(field.getBytes());
+        assertNotNull(field.getString());
+
+        // DataManager.getField()
+        field = datamanager.getField(FieldType.UNKNOWN, table, name, "1");
+        assertTrue(field.getBool());
+        assertNotNull(field.getBytes());
+        assertNotNull(field.getString());
+
+        // DataManager.get<Kinda>Field()
+        assertNotNull(datamanager.getBlobField(table, name, "1"));
+        assertNotNull(datamanager.getStringField(table, name, "1"));
+
+        // DataRow.get<Kinda>Field()
+        assertTrue(row.getBoolField(name));
+        assertNotNull(row.getBinaryField(name));
+        assertNotNull(row.getStringField(name));
+    }
+
     public void testTemplate() throws SQLException {
         final String name = "";
         DataRow row = datamanager.getResults("SELECT `" + name + "` FROM `" + table + "` LIMIT 1").getFirstResult();
